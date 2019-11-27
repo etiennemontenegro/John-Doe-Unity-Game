d4b55af262097d2c1cf45a84d70d22aa1728aa55
samtremblo <samtremblay.exe@gmail.com>
2019-11-26 19:38:18 -0500

    I did some shit

---
+++ Assets/Script/ObjectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject SpawnedObject;
+    public bool hasSpawned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasSpawned)
+        {
+            Instantiate(SpawnedObject);
+            hasSpawned = true;
+        }
+
+
+    }
+
+  
+}
