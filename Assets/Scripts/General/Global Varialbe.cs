3cd3175dfc610d9f324e9ea6b16f98561ee97f6b
Etienne <etienne_montenegro@hotmail.com>
2019-11-30 10:52:06 -0500

    uptated variable global script

---
+++ Assets/Scripts/General/Global Varialbe.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whatever : MonoBehaviour
+{
+
+    //GENERAL
+
+    public static int sceneIndex = 0; // 0= house 1=travel 2= work 
+    
+    public static int day = 0;
+    public static bool morning = true;
+    public static bool weekend = false;
+    public static bool menu = true;
+
+    public static int wallet = 0; //argent dans ton compte
+
+    
+
+    //HOUSE
+
+
+    //WORK
+
+    public static int paycheck = 0; //salaire verser a chaque semaine
+
+    //TRAVELLING
+
+
+    //STORE
+
+    public static bool inStore = false; 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
