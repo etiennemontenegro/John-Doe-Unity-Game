527372f759b6bfc0a2fc0f346df0a066eb21152b
Etienne <etienne_montenegro@hotmail.com>
2019-11-30 14:20:01 -0500

    first phase gamelogic

---
+++ Assets/Scripts/General/PersistantManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistantManager : MonoBehaviour
+{
+
+    public static PersistantManager Instance { get; private set; }
+
+    // GameObject lvlChanger = GameObject.Find
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
--- Assets/Scripts/General/SceneHandler.cs
+++ Assets/Scripts/General/SceneHandler.cs
@@ -1,44 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 
 
 public class SceneHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        
+        SceneManager.LoadScene(0, LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+      //  if(Input.GetKeyDown(KeyCode.Space)) {GlobalVariable.goToWork = !GlobalVariable.goToWork;}
+      
+        if( GlobalVariable.sceneIndex == 0 && GlobalVariable.goToWork)
+        {
+            scene0To1();
+        }
+
+        if (GlobalVariable.sceneIndex == 1 && GlobalVariable.atWork)
+        {
+            scene1To2();
+        }
+
+        if (GlobalVariable.sceneIndex == 2 && GlobalVariable.workIsDone)
+        {
+            scene2To1();
+        }
+
+        if (GlobalVariable.sceneIndex == 1 && GlobalVariable.backHome)
+        {
+            scene1To0();
+        }
+
     }
 
     void scene0To1() // HOUSE TO TRAVEL
     {
-        if( GlobalVariable.goToWork)
+        if(GlobalVariable.goToWork)
         {
-            GlobalVariable.sceneIndex = 1
-            
+            GlobalVariable.sceneIndex = 1;
+           
+            SwitchScene(GlobalVariable.sceneIndex, GlobalVariable.sceneIndex - 1);
         }
     }
 
-    void scene1To2()
+    void scene1To2() //TRAVEL TO WORK
     {
+        if( GlobalVariable.walkToWork)
+        {
+            GlobalVariable.sceneIndex = 2;
+            SwitchScene(GlobalVariable.sceneIndex, GlobalVariable.sceneIndex - 1);
+        }
     }
 
-    void scene2To1()
+    void scene2To1() //WORK TO TRAVEL
     {
+        if (GlobalVariable.workIsDone)
+        {
+            GlobalVariable.sceneIndex = 1;
+            SwitchScene(GlobalVariable.sceneIndex, GlobalVariable.sceneIndex + 1);
+        }
     }
 
-    void scene1To0()
+    void scene1To0() //TRAVEL TO HOUSE
     {
+        if (GlobalVariable.backHome)
+        {
+            GlobalVariable.sceneIndex = 0;
+            SwitchScene(GlobalVariable.sceneIndex, GlobalVariable.sceneIndex + 1);
+        }
     }
 
+    void SwitchScene( int load , int unload)
+    {
+        SceneManager.UnloadSceneAsync(unload);  // Async beacause of weird error check if bug 
+        SceneManager.LoadScene(load, LoadSceneMode.Additive);
+        
 
+    }
 }
