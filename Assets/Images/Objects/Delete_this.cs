cf3659378557b447c5e56a9512cd49dfc4a1ccfe
Anthony Trépanier <anthony.trepanier@hotmail.com>
2019-11-30 12:14:59 -0500

    Revert "Global Var antho, door system, raycast 2d hit"
    This reverts commit a9184b597632fe02587d887c2035eff796cd48a7.

--- Assets/Images/Objects/Delete_this.cs
+++ Assets/Images/Objects/Delete_this.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Delete_this : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
