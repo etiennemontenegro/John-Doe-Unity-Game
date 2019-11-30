0779c1d592ff1c23cd410c71be2f781e3953befe
tetreaultc <charles.tetreault@gmail.com>
2019-11-30 15:59:47 -0500

    Fade Animator on PrefabGameLogic

---
+++ Assets/Scripts/CharlesTScripts/FadeAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FadeAnimation : MonoBehaviour
+{
+
+    public Animator fadeAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayFadeIn()
+    {
+        fadeAnimator.SetBool("PlayFade", true);
+    }
+
+    public void PlayFadeOut()
+    {
+        fadeAnimator.SetBool("PlayFade", false);
+    }
+}
