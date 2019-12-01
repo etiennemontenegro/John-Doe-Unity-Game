fb755405245a0c6689397e0c82c4073ab65bbd34
cegMo <46608529+cegMo@users.noreply.github.com>
2019-12-01 18:58:21 -0500

    Store Completed

--- Assets/Scripts/UI/ItemNameList.cs
+++ Assets/Scripts/UI/ItemNameList.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 public class ItemNameList : MonoBehaviour
 {
     [SerializeField]
-    public static string[] GeneralitemNames = { "TV 1", "TV 2", "TV 3", "Lumiere 1", "Lumiere 2", "Table 1", "Table 2", "Table 3", "Mur 1", "Mur 2", "Mur 3", "Poster 1", "Poster 2", "Poster 3", "Chaise 1", "Chaise 2", "Chaise 3", "Plancher 1", "Plancher 2", "Plancher 3", "Plante 1", "Trophe 1", "Trophe 2", "Sofa 1", "Sofa 2", "Sofa 3", "Lit 1", "Lit 2", "Lit 3", "Figidaire 2" };
+    public static string[] GeneralitemNames = { "TV 1", "TV 2", "TV 3",
+                                              "Lumiere 1", "Lumiere 2",
+                                       "Table 1", "Table 2", "Table 3",
+                                                      "Mur 1", "Mur 2",
+                                    "Poster 1", "Poster 2", "Poster 3",
+                                    "Chaise 1", "Chaise 2", "Chaise 3",
+                                            "Plancher 1", "Plancher 2",
+                                                "Plante 1", "Plante 2",
+                                                "Trophe 1", "Trophe 2",
+                                          "Sofa 1", "Sofa 2", "Sofa 3",
+                                             "Lit 1", "Lit 2", "Lit 3",
+                                                         "Figidaire 2",
+                                                             "AMAZING" };
 }
