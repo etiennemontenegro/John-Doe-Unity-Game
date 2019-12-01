a90c1fb81bb6ba6e0505dffc99028fc706e77523
tetreaultc <charles.tetreault@gmail.com>
2019-11-30 23:08:45 -0500

    PrefabGameLogic + WeekTextHandler

--- Assets/WeekTextHandler.cs
+++ Assets/WeekTextHandler.cs
@@ -6,16 +6,40 @@ using UnityEngine.UI;
 public class WeekTextHandler : MonoBehaviour
 {
     public Text dayTextField;
-    private string[] dayNames = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
-    // Start is called before the first frame update
+    private string[] dayNames = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Weekend" };
+    
     void Start()
     {
         dayTextField.text = dayNames[GlobalVariable.day];
+        // To Fade Out Text
+        StartCoroutine(FadeTextToZeroAlpha(1f, dayTextField.GetComponent<Text>()));
+
+        // To Fade In Text
+        //StartCoroutine(FadeTextToFullAlpha(1f, dayTextField.GetComponent<Text>()));
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
     }
 }
