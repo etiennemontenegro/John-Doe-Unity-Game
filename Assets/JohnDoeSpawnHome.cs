ab8f2df663308fd33fdc1ce0806169b7e0a8feb5
tetreaultc <charles.tetreault@gmail.com>
2019-12-01 18:28:16 -0500

    John Doe Spawn Randomly

---
+++ Assets/JohnDoeSpawnHome.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JohnDoeSpawnHome : MonoBehaviour
+{
+    private float facingSide;
+    private float scaleX;
+
+    void Start()
+    {      
+        gameObject.transform.position = new Vector3(Random.Range(-7.5f, 7.5f), -4.75f, 0);
+
+        facingSide = Random.Range(-1f, 1f);
+        if (facingSide > 0)
+        {
+            scaleX = 0.8f;
+        } else
+        {
+            scaleX = -0.8f;            
+        }
+        gameObject.transform.localScale = new Vector3(scaleX, 0.8f, 1);
+    }
+}
