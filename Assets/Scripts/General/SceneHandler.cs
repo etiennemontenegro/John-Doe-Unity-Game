5ea1e2dc321d73904567858a69b7b64341fba1bf
tetreaultc <charles.tetreault@gmail.com>
2019-11-30 22:44:16 -0500

    WeekTextHandler

--- Assets/Scripts/General/SceneHandler.cs
+++ Assets/Scripts/General/SceneHandler.cs
@@ -2,20 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
-
 
 public class SceneHandler : MonoBehaviour
 {
 
-    public WeekHandler week;
-    public Text dayTextField;
-    private string[] dayNames = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+    public WeekHandler week;    
 
     void Start()
     {
-        dayTextField.text = dayNames[GlobalVariable.day];
+        
     }
 
     // Update is called once per frame
@@ -27,31 +22,27 @@ public class SceneHandler : MonoBehaviour
     public void scene0To1() // HOUSE TO TRAVEL
     {
      
-            GlobalVariable.sceneIndex = 1;
-           
-            SwitchScene(GlobalVariable.sceneIndex );
-            GlobalVariable.walkToWork = true;
+        GlobalVariable.sceneIndex = 1;           
+        SwitchScene(GlobalVariable.sceneIndex );
+        GlobalVariable.walkToWork = true;
     
     }
 
    public void scene1To2() //TRAVEL TO WORK
-    {
-        
-       
+    {            
         GlobalVariable.sceneIndex = 2;
         SwitchScene(GlobalVariable.sceneIndex);
         GlobalVariable.walkToWork = false;
         GlobalVariable.atWork = true;
         WeekHandler.walkTime = WeekHandler._walkTime;
         Debug.Log("Reset walk timer : " + WeekHandler.walkTime);
-
-}
+    }
 
    public void scene2To1() //WORK TO TRAVEL
     {
        
-            GlobalVariable.sceneIndex = 1;
-            SwitchScene(GlobalVariable.sceneIndex);
+        GlobalVariable.sceneIndex = 1;
+        SwitchScene(GlobalVariable.sceneIndex);
         WeekHandler.workTime = WeekHandler._workTime;
         Debug.Log("Reset work timer : " + WeekHandler.workTime);
 
@@ -60,8 +51,8 @@ public class SceneHandler : MonoBehaviour
    public void scene1To0() //TRAVEL TO HOUSE
     {
        
-            GlobalVariable.sceneIndex = 0;
-            SwitchScene(GlobalVariable.sceneIndex);
+        GlobalVariable.sceneIndex = 0;
+        SwitchScene(GlobalVariable.sceneIndex);
         GlobalVariable.hasArrived = false;
         GlobalVariable.backHome = false;
         WeekHandler.walkTime = WeekHandler._walkTime;
@@ -71,10 +62,7 @@ public class SceneHandler : MonoBehaviour
     }
 
     void SwitchScene( int load )
-    {
-      
-        SceneManager.LoadScene(load);
-        
-
+    {      
+        SceneManager.LoadScene(load);   
     }
 }
