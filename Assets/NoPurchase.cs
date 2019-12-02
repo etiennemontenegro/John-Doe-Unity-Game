e7eea433878d736499d6996f12b92ab5d02ad030
samtremblo <samtremblay.exe@gmail.com>
2019-12-02 10:38:54 -0500

    Added empty panier message

---
+++ Assets/NoPurchase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+public class NoPurchase : MonoBehaviour
+ 
+{
+    public Text orderText;
+    public InventoryManager Sele;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(InventoryManager.totalPrice == 0)
+        {
+            GetComponent<UnityEngine.UI.Text>().text = "Votre panier est vide!";
+        }
+      
+    }
+}
