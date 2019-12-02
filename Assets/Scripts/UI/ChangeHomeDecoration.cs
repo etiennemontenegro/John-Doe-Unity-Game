b6202a6f399e30338065892b1d9a1cf8b7fc7e00
Anthony Trépanier <anthony.trepanier@hotmail.com>
2019-12-02 10:46:55 -0500

    adjust some shit to the home scene

---
+++ Assets/Scripts/UI/ChangeHomeDecoration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHomeDecoration : MonoBehaviour
+{
+
+    public GameObject TV;
+    public Sprite newSpriteElement;
+
+
+    public bool isBought = true;
+
+    public GameObject oldStore;
+   // public GameObject newStore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        oldStore = GameObject.Find("StoreUI");
+        newSpriteElement = oldStore.GetComponent<StoreSetup>().storeItems[0].itemSprite;
+
+        if (isBought == true)
+        {
+            TV.GetComponent<SpriteRenderer>().sprite = newSpriteElement;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
