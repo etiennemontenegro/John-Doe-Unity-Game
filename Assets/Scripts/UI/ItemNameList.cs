ee8561eddb7b5309fbdc53d2ebfce521aab83355
Karim Zniber <karim.zniber@hotmail.com>
2019-12-03 09:58:09 -0500

    Noms des objets Final

--- Assets/Scripts/UI/ItemNameList.cs
+++ Assets/Scripts/UI/ItemNameList.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class ItemNameList : MonoBehaviour
 {
     [SerializeField]
-    public static string[] GeneralitemNames = { "CRTTV 13po", "TCL720p 32po", "LG4K UHD 49po",
+    public static string[] GeneralitemNames = { "Magasonic CRT TV 13po", "TCL 720p 32po", "LG 4K UHD 49po",
                                               "Lampadaire usagee", "Lampadaire plaque or",
                                        "Table basse en bois", "Table moderne noire", "Table antique",
-                                                      "Tapisserie", "Tapisserie",
-                                    "Affiches", "Peinture", "Joconde",
+                                                      "Tapisserie Bleu Canard", "Tapisserie Decomurale",
+                                    "Affiches 20x20 po", "Peinture Abstraite", "La Joconde",
                                     "Chaise hetre", "Chaise antique", "Chaise Fitz et Co.",
                                             "Plancher bois Franc", "Tapis artisanal",
                                                 "Plante Costa Naar", "Cactus SP4 australien",
                                                 "Statuette faux or", "Trophee Oscar num 1",
                                           "Fauteuil Inclinable", "Sofa contemporain", "Sofa Gibyag",
                                              "Lit pliant", "Lit appoint 1 place", "Lit de luxe",
-                                                         "Refrigerateur",
+                                                         "Refrigerateur francais",
                                                              "AMAZING plaquee or" };
 }
